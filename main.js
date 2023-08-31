e54ffb56284195e6162adbabe982e8f394063f9f
LorenzoMoiana <136532510+LorenzoMoiana@users.noreply.github.com>
2023-08-31 18:52:14 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var fullName = firstName.concat(lastName);
+  
+   var lenghtFullName = fullName.lenght;
+
+   var replaceName = nameOne.replace("Rathord", "Cena");
+
+   var makingLowerCase = capitalName.toLowerCase();
+
+   var squareRoot = Math.sqrt(16);
+
+   var maximumNumber = Math.max(15,10);
+
+   var powerEg1 = math.pow(4,2);
